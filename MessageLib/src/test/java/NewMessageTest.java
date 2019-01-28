bcc33521ce6ba3d8c08960a4a77b33ce0773506d
khalchev91 <kcheverria9@gmail.com>
2019-01-27 21:04:35 -0500

    Added Unit test

--- MessageLib/src/test/java/NewMessageTest.java
+++ MessageLib/src/test/java/NewMessageTest.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import messages.TextMessage;
+import org.junit.Before;
+import org.junit.Test;
+import phones.Android;
+import phones.IPhone;
+import phones.Phone;
+import storageproviders.Google;
+import storageproviders.ICloud;
 
 public class NewMessageTest {
+
+    private Phone iPhone;
+    private Phone pixel;
+
+    @Before
+    public void setUp(){
+        iPhone= new IPhone(new ICloud());
+        pixel = new Android(new Google());
+    }
+
+    @Test
+    public void iPhoneMessageShouldSendTest(){
+        String message = "Some test message for iPhone";
+        ((IPhone) iPhone).send(new TextMessage(message));
+        assert(iPhone.getStorage().retrieveAll().size()>0);
+    }
+
+    @Test
+    public void AndroidMessageShouldSendTest(){
+        String message = "Some test message for Pixel";
+        ((Android) pixel).send(new TextMessage(message));
+        assert(pixel.getStorage().retrieveAll().size()>0);
+    }
 }
